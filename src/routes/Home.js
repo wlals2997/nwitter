587e2475bf3b4bd4462f924baa7ecbd339a087b6
wlals2997 <wlals2997@naver.com>
2022-07-11 14:58:09 +0900

    onSubmit,firestore error

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,40 @@
-import React from "react";
-const Home=()=><span>Home</span>;
+import { dbService } from 'fbase';
+import { collection, addDoc, serverTimestamp } from 'firebase/firestore';
+import React, { useState } from 'react';
+const Home = () => {
+  const [nweet, setNweet] = useState('');
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const docRef = await addDoc(collection(dbService, 'nweets'), {
+        nweet,
+        createdAt: serverTimestamp(),
+      });
+      console.log('Document written with ID: ', docRef.id);
+    } catch (error) {
+      console.error('Error adding document: ', error);
+    }
+
+    setNweet('');
+  };
+  const onChange = (e) => {
+    const { value } = e.target;
+    setNweet(value);
+  };
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          value={nweet}
+          onChange={onChange}
+          type='text'
+          placeholder="What's happening?"
+          maxLength={120}
+        ></input>
+        <input type='submit' value='Nweet'></input>
+      </form>
+    </>
+  );
+};
+
 export default Home;
